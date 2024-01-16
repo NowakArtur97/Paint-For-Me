affa30590509ae1951d157053e99b7f93e5953a0
NowakArtur97 <nowart97@gmail.com>
2024-01-16 21:58:50 +0100

    Setup the button to clear the canvas

--- paint.js
+++ paint.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const brushSizeInputElement = document.querySelector(".control__input");
   const canvas = document.querySelector(".canvas");
+  const clearCanvasBtnElement = document.querySelector("#clean_canvas");
   const canvasSize = canvas.getBoundingClientRect();
   const ctx = setupCanvas(canvas, brushSizeInputElement);
   let isDrawing = false;
@@ -34,16 +35,20 @@ document.addEventListener("DOMContentLoaded", () => {
     ctx.stroke();
   }
 
+  function clearCanvas() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
   canvas.addEventListener("mousedown", startDrawing);
   canvas.addEventListener("mousemove", draw);
   canvas.addEventListener("mouseup", () => (isDrawing = false));
   canvas.addEventListener("mouseout", () => (isDrawing = false));
-
+  clearCanvasBtnElement.addEventListener("click", clearCanvas);
   brushSizeInputElement.addEventListener(
     "input",
     () => (ctx.lineWidth = brushSizeInputElement.value)
   );
-
   document.querySelectorAll(".color").forEach((colorElement) => {
     const color = colorElement.getAttribute("data-color");
     colorElement.style.backgroundColor = color;
