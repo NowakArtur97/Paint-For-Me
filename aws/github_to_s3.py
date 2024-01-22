1a8daaaa9d9723ba109b6218ce67dae82f6e5c82
NowakArtur97 <nowart97@gmail.com>
2024-01-22 19:48:19 +0100

    Resolve content type when uploading file to S3 bucket

--- aws/github_to_s3.py
+++ aws/github_to_s3.py
@@ -7,7 +7,7 @@ import cfnresponse
 s3 = boto3.resource('s3')
 BUCKET = os.environ['BUCKET_NAME']
 GITHUB_URL = os.environ['GITHUB_URL']
-FILES_TO_COPY = os.environ['FILES_TO_COPY']
+FILES_TO_COPY = os.environ['FILES_TO_COPY'].split(",")
 
 def save_to_local(url):
     urlPath = urlparse(url).path
@@ -16,22 +16,58 @@ def save_to_local(url):
     urllib.request.urlretrieve(url, filePath)
     return filePath
 
-def copy_to_s3(url):
+def copy_to_s3(url, contentType):
     filePath = save_to_local(url)
     fileName = os.path.basename(filePath)
-    s3.meta.client.upload_file(filePath, BUCKET, fileName)
-    s3.Object(BUCKET, fileName).put(Body=open(filePath, 'rb'), ContentType="application/zip")
+    s3.Object(BUCKET, fileName).put(Body=open(filePath, 'rb'), ContentType=contentType)
+
+def resolve_content_type(file):
+    extension = file.split(".")[1]
+    if extension == "html":
+        return "text/html"
+    elif extension == "css":
+        return "text/css"
+    elif extension == "js":
+        return "text/javascript"
+    elif extension == "py":
+        return "text/x-python"
+    elif extension in ["jpeg", "jpg"]:
+        return "image/jpeg"
+    elif extension == "png":
+        return "image/png"
+    elif extension == "tiff":
+        return "image/tiff"
+    elif extension == "bmp":
+        return "image/bmp"
+    elif extension == "gif":
+        return "image/gif"
+    elif extension in ["svg", "xml"]:
+        return "image/svg+xml"
+    elif extension in ["mp3", "wav", "ogg"]:
+        return "audio/mpeg"
+    elif extension == "pdf":
+        return "application/pdf"
+    elif extension == "zip":
+        return "application/zip"
+    elif extension == "yaml":
+        return "binary/octet-stream"
+    else:
+        return "text/plain"
 
 def lambda_handler(event, context):
     responseData = {}
     requestType = event['RequestType']
     try:
         if requestType == 'Create':
-            for fileFromGitHub in FILES_TO_COPY:
-                copy_to_s3(GITHUB_URL + "/" + fileFromGitHub)
-                print("Successfully copied file: " + fileFromGitHub + " to bucket: " + BUCKET)
+            for fileToCopy in FILES_TO_COPY:
+                fileOnGitHub = GITHUB_URL + "/" + fileToCopy
+                print("File to copy: " + fileToCopy)
+                print("URL to file: " + fileOnGitHub)
+                contentType = resolve_content_type(fileToCopy)
+                copy_to_s3(fileOnGitHub, contentType)
+                print("Successfully copied file: " + fileToCopy + " to bucket: " + BUCKET)
         cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData)
     except Exception as e:
-        print("Exception")
+        print("Exception when copying files from GitHub to S3 bucket: " + BUCKET)
         print(e)
         cfnresponse.send(event, context, cfnresponse.FAILED, responseData)
